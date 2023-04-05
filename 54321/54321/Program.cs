827dab774bd166c7d1fea87698fdeb9e728cec1d
Nasrin F. Asadli <nasrinfa@code.local>
2023-04-05 16:16:14 +0400

    added 54321

---
+++ 54321/54321/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Enter a number");
+string strnum = Console.ReadLine();
+mirror(strnum);
+
+void mirror(string num)
+{
+    try
+    {
+        int number = int.Parse(num);
+        int nummir = 0;
+        while (number / 1 >= 1)
+        {
+        nummir = nummir * 10 + number % 10;
+        number /= 10;
+        }
+        Console.WriteLine(nummir);
+        
+    }
+    catch (Exception)
+    { Console.WriteLine("I said NUMBER! Use your brain >:("); }
+}
